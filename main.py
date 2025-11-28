3812dd52d81e2b2b24fd38d2a4f4ec5f8d0a0fce
David Leon <jelid.leon@gmail.com>
2025-11-27 18:46:21 -0600

    Add typer to organize the puzzles

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from advent-of-code-2025!")
+import typer
 
+def main(name: str = "World"):
+    print(f"Hello {name} from advent-of-code-2025!")
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
